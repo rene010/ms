1da9b6abebe43c1fe6a1152d21ab4e952d40ebac
René H.P <rene010rene@gmail.com>
2025-01-06 21:42:02 +0100

    Update rm-notice.sh

--- rm-notice.sh
+++ rm-notice.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Step 1: Change to the directory containing proxmoxlib.js and make a backup copy
+# Change to the directory containing proxmoxlib.js and make a backup copy
 cd /usr/share/javascript/proxmox-widget-toolkit/ || { echo "Failed to change directory"; exit 1; }
 cp proxmoxlib.js proxmoxlib.js.bak
 
-# Step 2: Find the lines to be commented and modify the file
+# Find the lines to be commented and modify the file
 sed -i '/res === null.*res === undefined.*res/{ 
   s/^/\/\// 
   n 
